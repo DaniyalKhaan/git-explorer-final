de10e94ee45b4cb32496f82a71d98140d778f0ef
Daniyal Khan <124635608+DaniyalKhaan@users.noreply.github.com>
2025-01-07 03:00:35 +0500

    added basename to fix routing issue

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import Navbar from "./components/Navbar";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/git-explorer-final">
       <Navbar />
       <App />
     </BrowserRouter>
